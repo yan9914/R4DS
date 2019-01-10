8a3b6d801023fc20a78e0a7ceb6036bf80ca2219
yan9914 <31344747+yan9914@users.noreply.github.com>
2019-01-10 21:57:47 +0800

    Add files via upload

---
+++ r4ds_ch2.R
@@ -0,0 +1,13 @@
+# exercise
+
+# 1
+# typing error (i,1)
+
+# 2
+library(tidyverse)
+ggplot(mpg) +
+  geom_point(aes(displ, hwy))
+filter(mpg, cyl == 8)
+filter(diamonds, carat > 3)
+
+# 3 Help -> Keyboard Shortcuts Help
